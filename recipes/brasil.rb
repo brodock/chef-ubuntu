5ed3259010ba53af081e51acb541a5bd7fb7b172
Gabriel Mazetto <brodock@gmail.com>
2012-12-03 05:33:18 -0200

    Updated again to make recipes work

--- recipes/brasil.rb
+++ recipes/brasil.rb
@@ -1,1 +1,5 @@
+
+node.normal[:ubuntu][:archive_url]  = "http://espelhos.edugraf.ufsc.br/ubuntu"
+node.normal[:ubuntu][:security_url] = "http://espelhos.edugraf.ufsc.br/ubuntu"
+
 include_recipe "ubuntu::default"
