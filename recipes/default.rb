dac661bd99d25de90e0939e386fca250d136b3fa
Gabriel Mazetto <brodock@gmail.com>
2013-01-18 14:09:49 -0200

    Added language-pt to be installed

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,6 +26,8 @@ template "/etc/apt/sources.list" do
   source "sources.list.erb"
 end
 
+package('language-pack-pt') { action :install }
+
 if node[:ubuntu][:unattended_upgrades]
   package('unattended-upgrades') { action :install }
 else
