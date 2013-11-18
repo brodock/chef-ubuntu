1bc1d6d8f60b57f8453de6f95e383e95d7473ba8
Gabriel Mazetto <brodock@gmail.com>
2013-11-18 00:18:41 -0200

    Added digitalocean repositories

---
+++ recipes/digitalocean.rb
@@ -0,0 +1,5 @@
+
+node.normal[:ubuntu][:archive_url]  = "http://mirrors.digitalocean.com/ubuntu"
+node.normal[:ubuntu][:security_url] = "http://mirrors.digitalocean.com/ubuntu"
+
+include_recipe "ubuntu::default"
